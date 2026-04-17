a2d555f20f9d3abb77112fb66281d8f1a7a434fd
Igor Tiburcio Cavalcanti <igor.tiburcio@juridico.ai>
2026-04-16 22:15:34 -0300

    feat: load environment variables from .env file into system properties at application startup

--- src/main/java/com/duduisonfire/chat_bot/ChatBotApplication.java
+++ src/main/java/com/duduisonfire/chat_bot/ChatBotApplication.java
@@ -3,10 +3,15 @@ package com.duduisonfire.chat_bot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class ChatBotApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
 		SpringApplication.run(ChatBotApplication.class, args);
 	}
 
